a14573ad4c4a402ac07e7ecdea4d0d0ad60d2adc
Cem Kıray <jamcry@hotmail.com>
2023-08-01 17:06:56 +0200

    Update apiConstants.ts again

--- src/api/apiConstants.ts
+++ src/api/apiConstants.ts
@@ -1,5 +1,5 @@
 // TODO: ideally get this from env
-const API_BASE_URL = "https://jambin-backend.onrender.com/";
+const API_BASE_URL = "https://jambin-backend.onrender.com";
 // const API_BASE_URL = "http://localhost:8000";
 
 const DEFAULT_API_REQ_HEADERS = {
