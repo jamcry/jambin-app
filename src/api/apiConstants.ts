60db7583f2ce1a5425d61c25dbaa10166c0e5323
Cem Kıray <jamcry@hotmail.com>
2023-08-01 16:59:31 +0200

    Update apiConstants.ts

--- src/api/apiConstants.ts
+++ src/api/apiConstants.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://secret-thicket-37395.herokuapp.com";
+// TODO: ideally get this from env
+const API_BASE_URL = "https://jambin-backend.onrender.com/";
 // const API_BASE_URL = "http://localhost:8000";
 
 const DEFAULT_API_REQ_HEADERS = {
